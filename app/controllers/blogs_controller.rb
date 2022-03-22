5f0d8cc5c989b89338ca1b6a1d2e3deb45e4e3bd
IBRAHIMA <hadi.ibrahima8@gmail.com>
2022-03-22 07:12:28 +0100

    pratiquerails

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -60,21 +60,123 @@
 
 
 
+
+
+#class BlogsController < ApplicationController
+#  def index
+#     @blogs = Blog.all
+#   end
+
+#    def new
+#    @blog = Blog.new
+#   end
+
+#  def create
+#    Blog.create(blog_params)
+#    redirect_to new_blog_path
+#  end
+
+  # Définir
+#  def show
+#  @blog = Blog.find(params[:id])
+#  end
+
+#  private
+
+#  def blog_params
+#    params.require(:blog).permit(:title, :content)
+#  end
+#end
+
+
+
+
+
+
+
 class BlogsController < ApplicationController
-  #省略
+before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
-  def create
-    Blog.create(blog_params)
-    redirect_to new_blog_path
-  end
+  def index
+     @blogs = Blog.all
+   end
 
-  # 定義する
+    def new
+    @blog = Blog.new
+   end
+
+
+
+   #def create
+    #   @blog = Blog.new(blog_params)
+    #   if @blog.save
+    #     redirect_to blogs_path, notice: "J'ai créé un blog！"
+    #   else
+    #     render :new
+    #   end
+     #end
+
+
+
+
+     def create
+         @blog = Blog.new(blog_params)
+         if params[:back]
+           render :new
+         else
+           if @blog.save
+             redirect_to blogs_path, notice: "j'ai créé un blog！"
+           else
+             render :new
+           end
+         end
+       end
+
+
+
+  # Définir
   def show
+  #@blog = Blog.find(params[:id])
   end
+
+  def edit
+    # @blog = Blog.find(params[:id])   
+   end
+
+
+  def update
+     #@blog = Blog.find(params[:id])
+     if @blog.update(blog_params)
+       redirect_to blogs_path, notice: "J'ai édité le blog!"
+     else
+       render :edit
+     end
+   end
+
+   def destroy
+       @blog.destroy
+       redirect_to blogs_path, notice:"J'ai supprimé le blog！"
+   end
+
+
+   def confirm
+      @blog = Blog.new(blog_params)
+    end
+
+
+    def confirm
+        @blog = Blog.new(blog_params)
+        render :new if @blog.invalid?
+      end
+
 
   private
 
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
+  # Ajouter une méthode pour obtenir une valeur avec id comme clé
+ def set_blog
+   @blog = Blog.find(params[:id])
+ end
 end
